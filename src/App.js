a2abf17878d61c79bceb703b55bb47db836be46c
Sahil Nare <37807526+sahilnare@users.noreply.github.com>
2019-12-13 15:42:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import PostList from './components/PostList';
 import EditPost from './components/EditPost';
 import CreatePost from './components/CreatePost';
 import CreateUser from './components/CreateUser';
-
 
 function App() {
   return (
